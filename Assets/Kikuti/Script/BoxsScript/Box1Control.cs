59a2a407a9301c060d2dc42781a555f4e6cdc8c1
PG22-01 <20cge0011st@edu.sendai-com.ac.jp>
2022-07-23 17:25:44 +0900

    菊池１０回目

--- Assets/Kikuti/Script/BoxsScript/Box1Control.cs
+++ Assets/Kikuti/Script/BoxsScript/Box1Control.cs
@@ -35,101 +35,22 @@ public class Box1Control : MonoBehaviour
     {
         if (other.gameObject.tag == "Bullet")
         {
-            Instantiate(openSound, this.transform.position, this.transform.rotation);//効果音
-            Destroy(gameObject);
-
-            int i=0;//while内をループするための処理
-            while (decision = true)
-            {
-                float rnd = Random.Range(0, 1f);
-
-                //攻撃範囲UPを買っていないならドロップ数＝0に設定
-                if (PlayerControl.shopRange == false)
-                {
-                    GameManager.attackRange=0;
-                }
-                //防御力UPを買っていないならドロップ数＝0に設定
-                if (PlayerControl.shopDefense == false)
-                {
-                    GameManager.defenseUp=0;
-                }
-
-                //すべての強化本のドロップ数が上限を超えたら資源を落とす
-                if (GameManager.attackUp==0&& GameManager.attackSpeedUp == 0 && GameManager.speedUp == 0 && GameManager.attackRange == 0 && GameManager.defenseUp == 0 )
-                { 
-                    Stone();
-                    break;
-                }
-                //攻撃力UPドロップ
-                if (rnd <= 0.3f) 
-                {
-                    if (GameManager.attackUp > 0)
-                    {
-                        Instantiate(attackUpBook, this.transform.position, this.transform.rotation);
-                        GameManager.attackUp -= 1;
-                        break;
-
-                    }
-                    else { i+=1; }//while内をループするための処理
-
-                }
-                //攻撃速度UPドロップ
-                else if (rnd <= 0.5f)
-                {
-                    if (GameManager.attackSpeedUp > 0)//ドロップ数上限超えてなければ実行
-                    {
-                        Instantiate(attackSpeedBook, this.transform.position, this.transform.rotation);
-                        GameManager.attackSpeedUp -= 1;
-                        break;
-                    }
-                    else { i += 1; }
-                }
-                //移動速度UPドロップ
-                else if (rnd <= 0.7f)
-                {
-                    if (GameManager.speedUp > 0)//ドロップ数上限超えてなければ実行
-                    {
-                        Instantiate(speedBook, this.transform.position, this.transform.rotation);
-                        GameManager.speedUp -= 1;
-                        break;
-                    }
-                    else { i += 1; }
-                }
-                //攻撃範囲UPドロップ
-                else if (rnd <= 0.8f)
-                {
-                    if (PlayerControl.shopRange == true)//ショップで購入していれば実行
-                    {
-                        if (GameManager.attackRange > 0)//ドロップ数上限超えてなければ実行
-                        {
-                            Instantiate(attackRangeBook, this.transform.position, this.transform.rotation);
-                            GameManager.attackRange -= 1;
-                            break;
-                        }
-                        else { i += 1; }
-                    }
-                }
-                //防御力UPドロップ
-                else
-                {
-                    if (PlayerControl.shopDefense == true)//ショップで購入していれば実行
-                    {
-                        if (GameManager.defenseUp > 0)//ドロップ数上限超えてなければ実行
-                        {
-                            Instantiate(defenseBook, this.transform.position, this.transform.rotation);
-                            GameManager.defenseUp -= 1;
-                            break;
-                        }
-                        else { i += 1; }
-                    }
-                }
-            
-
-            }
+           Drop1();
 
         }
 
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Drop1();
+
+        }
+
+    }
+
     private void Stone()
     {
         for (int i = 0; i < 5; i++)
@@ -138,5 +59,100 @@ public class Box1Control : MonoBehaviour
         }
 
 
+    }
+
+    public void Drop1()
+    {
+        Instantiate(openSound, this.transform.position, this.transform.rotation);//効果音
+        Destroy(gameObject);
+
+        int i = 0;//while内をループするための処理
+        while (decision = true)
+        {
+            float rnd = Random.Range(0, 1f);
+
+            //攻撃範囲UPを買っていないならドロップ数＝0に設定
+            if (PlayerControl.shopRange == false)
+            {
+                GameManager.attackRange = 0;
+            }
+            //防御力UPを買っていないならドロップ数＝0に設定
+            if (PlayerControl.shopDefense == false)
+            {
+                GameManager.defenseUp = 0;
+            }
+
+            //すべての強化本のドロップ数が上限を超えたら資源を落とす
+            if (GameManager.attackUp == 0 && GameManager.attackSpeedUp == 0 && GameManager.speedUp == 0 && GameManager.attackRange == 0 && GameManager.defenseUp == 0)
+            {
+                Stone();
+                break;
+            }
+            //攻撃力UPドロップ
+            if (rnd <= 0.3f)
+            {
+                if (GameManager.attackUp > 0)
+                {
+                    Instantiate(attackUpBook, this.transform.position, this.transform.rotation);
+                    GameManager.attackUp -= 1;
+                    break;
+
+                }
+                else { i += 1; }//while内をループするための処理
+
+            }
+            //攻撃速度UPドロップ
+            else if (rnd <= 0.5f)
+            {
+                if (GameManager.attackSpeedUp > 0)//ドロップ数上限超えてなければ実行
+                {
+                    Instantiate(attackSpeedBook, this.transform.position, this.transform.rotation);
+                    GameManager.attackSpeedUp -= 1;
+                    break;
+                }
+                else { i += 1; }
+            }
+            //移動速度UPドロップ
+            else if (rnd <= 0.7f)
+            {
+                if (GameManager.speedUp > 0)//ドロップ数上限超えてなければ実行
+                {
+                    Instantiate(speedBook, this.transform.position, this.transform.rotation);
+                    GameManager.speedUp -= 1;
+                    break;
+                }
+                else { i += 1; }
+            }
+            //攻撃範囲UPドロップ
+            else if (rnd <= 0.8f)
+            {
+                if (PlayerControl.shopRange == true)//ショップで購入していれば実行
+                {
+                    if (GameManager.attackRange > 0)//ドロップ数上限超えてなければ実行
+                    {
+                        Instantiate(attackRangeBook, this.transform.position, this.transform.rotation);
+                        GameManager.attackRange -= 1;
+                        break;
+                    }
+                    else { i += 1; }
+                }
+            }
+            //防御力UPドロップ
+            else
+            {
+                if (PlayerControl.shopDefense == true)//ショップで購入していれば実行
+                {
+                    if (GameManager.defenseUp > 0)//ドロップ数上限超えてなければ実行
+                    {
+                        Instantiate(defenseBook, this.transform.position, this.transform.rotation);
+                        GameManager.defenseUp -= 1;
+                        break;
+                    }
+                    else { i += 1; }
+                }
+            }
+
+
+        }
     }
 }
